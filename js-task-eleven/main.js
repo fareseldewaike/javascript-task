4fc28046b8460137c78f0cc2ad460c08a1edda3d
DESKTOP-QBQJ070\USER <fareseldewaike@gmail.com>
2024-12-10 22:16:09 +0200

    colorized name

--- js-task-eleven/main.js
+++ js-task-eleven/main.js
@@ -5,14 +5,18 @@ let counter =0 ;
 document.querySelector("button").addEventListener("click", () => {
     counter++;
     let selectedGender = document.querySelector('input[name="gender"]:checked');
+    let selectedColor = document.querySelector("select").value;
         console.log("Name: " + namee.value);
         console.log("Age: " + age.value);
         console.log("Gender: " + selectedGender.value);
+        console.log("Color: " + selectedColor);
 
         document.cookie = `user=${namee.value};`;
         document.cookie = `age=${age.value};`;
         document.cookie = `gender=${selectedGender.value};`;
         document.cookie = `counter=${counter};`;
+        document.cookie = `color=${selectedColor};`;
+
 
         let userCookie = document.cookie.split(";");
         userCookie.forEach((cookie) => {
